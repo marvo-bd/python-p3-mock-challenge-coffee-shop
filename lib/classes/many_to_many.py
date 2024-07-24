0bb4b22111f9e421d5ab63b0031e81bacfb222f2
Marvin Kabira <marvobd@gmail.com>
2024-07-24 17:40:51 +0300

    updated mock code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,75 @@
+# many_to_many.py
+
 class Coffee:
     def __init__(self, name):
-        self.name = name
-        
+        self._name = name
+        self._orders = []
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        raise AttributeError("Name cannot be changed")
+    
+    def add_order(self, order):
+        if isinstance(order, Order):
+            self._orders.append(order)
+        else:
+            raise ValueError("Order must be an instance of Order")
+    
     def orders(self):
-        pass
+        return self._orders
     
     def customers(self):
-        pass
+        return list(set(order.customer for order in self._orders))
     
     def num_orders(self):
-        pass
+        return len(self._orders)
     
     def average_price(self):
-        pass
+        total_price = sum(order.price for order in self._orders)
+        return total_price / self.num_orders() if self.num_orders() > 0 else 0
 
 class Customer:
     def __init__(self, name):
-        self.name = name
-        
+        self._name = name
+        self._orders = []
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str) or not (1 <= len(new_name) <= 15):
+            raise ValueError("Name must be a string between 1 and 15 characters")
+        self._name = new_name
+    
+    def add_order(self, order):
+        if isinstance(order, Order):
+            self._orders.append(order)
+        else:
+            raise ValueError("Order must be an instance of Order")
+    
     def orders(self):
-        pass
+        return self._orders
     
     def coffees(self):
-        pass
-    
-    def create_order(self, coffee, price):
-        pass
-    
+        return list(set(order.coffee for order in self._orders))
+
+
 class Order:
     def __init__(self, customer, coffee, price):
+        if not isinstance(price, (int, float)) or not (1.0 <= price <= 10.0):
+            raise ValueError("Price must be between 1.0 and 10.0")
+        self._price = price
         self.customer = customer
         self.coffee = coffee
-        self.price = price
+        coffee.add_order(self)
+        customer.add_order(self)
+    
+    @property
+    def price(self):
+        return self._price
